56e75fb53b42d00aa24c32a52ecd6ab355b6be95
likaiji <1104310490@qq.com>
2018-04-01 23:56:49 +0800

    vue动态路由配置，以及注释路由#hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import GoodsList from '@/views/GoodsList'
 Vue.use(Router)
 
 export default new Router({
+    mode:'history', // 取消#
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      path: '/goods/:goodsId',  // 配置动态路由,动态变量
+      component: GoodsList
+    },
+      {
+          path: '/goods/:goodsId/user/:name',  // 配置动态路由,多个动态变量参数
+          component: GoodsList
+      }
   ]
 })
---
+++ src/views/GoodsList.vue
@@ -0,0 +1,24 @@
+<style rel="styleesheet">
+</style>
+
+<template>
+    <article>
+        这是商品列表页面
+        <span>{{$route.params.goodsId}}</span>
+        <span>{{$route.params.name}}</span>
+    </article>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        },
+        created() {
+        },
+        activated() {
+        },
+        methods: {},
+        components: {}
+    }
+</script>
